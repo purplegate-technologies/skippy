58df43aefbdae1a59df7f448040633717abe7bc7
geraldkachi <fitzgeraldkachi@gmail.com>
2022-09-20 03:01:00 +0100

    click out page per row

---
+++ src/components/PageRow/PageRow.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { ClickOutside } from '../../app/useClickOutside';
+// import { AIarrowdownbol, AIarrowup1Bol } from 'arvara-icons';
+
+interface IRows {
+	rowSize: number;
+	rowSizeChange?: (no: number) => void;
+	reset: () => void;
+}
+
+const PageRow = ({ rowSize, rowSizeChange, reset }: IRows) => {
+	const size = [5, 10, 20];
+	const [dropdown, setDropdown] = useState(false);
+	return (
+		<ClickOutside onclickoutside={() => setDropdown(false)}>
+			<div className="relative">
+				<div className="flex gap-2 items-center cursor-pointer">
+					<div
+						onClick={() => setDropdown(!dropdown)}
+						className="rounded border border-grey-beau px-2 py-1 flex items-center gap-1 cursor-pointer"
+					>
+						<p>{rowSize}</p>
+						<p className="text-black">
+							{dropdown ? (
+								// <AIarrowup1Bol width={18} height={18} />
+                                >
+							) : (
+								// <AIarrowdownbol width={18} height={18} />
+                                <
+							)}
+						</p>
+					</div>
+					<p className="text-md">Rows/page</p>
+				</div>
+				<div
+					className={`
+				w-12
+				flex
+				flex-col
+				gap-4
+				items-center
+				absolute
+				z-10
+				bg-white
+				py-2
+				mt-1
+				bottom-10
+				rounded
+				border
+				border-grey-beau
+				${dropdown ? 'block' : 'hidden'}`}
+				>
+					{size &&
+						size.map((item: number) => (
+							<p
+								key={item}
+								onClick={() => {
+									rowSizeChange && rowSizeChange(item);
+									setDropdown(false);
+									reset();
+								}}
+								className="hover:bg-white-azure rounded-lg cursor-pointer py-1 px-4"
+							>
+								{item}
+							</p>
+						))}
+				</div>
+			</div>
+		</ClickOutside>
+	);
+};
+
+export default PageRow;
---
+++ src/components/PageRow/dist/PageRow.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var useClickOutside_1 = require("../../app/useClickOutside");
+var PageRow = function (_a) {
+    var rowSize = _a.rowSize, rowSizeChange = _a.rowSizeChange, reset = _a.reset;
+    var size = [5, 10, 20];
+    var _b = react_1.useState(false), dropdown = _b[0], setDropdown = _b[1];
+    return (React.createElement(useClickOutside_1.ClickOutside, { onclickoutside: function () { return setDropdown(false); } },
+        React.createElement("div", { className: "relative" },
+            React.createElement("div", { className: "flex gap-2 items-center cursor-pointer" },
+                React.createElement("div", { onClick: function () { return setDropdown(!dropdown); }, className: "rounded border border-grey-beau px-2 py-1 flex items-center gap-1 cursor-pointer" },
+                    React.createElement("p", null, rowSize),
+                    React.createElement("p", { className: "text-black" }, dropdown ? (React.createElement(AIarrowup1Bol, { width: 18, height: 18 })) : (React.createElement(AIarrowdownbol, { width: 18, height: 18 })))),
+                React.createElement("p", { className: "text-md" }, "Rows/page")),
+            React.createElement("div", { className: "\n\t\t\t\tw-12\n\t\t\t\tflex\n\t\t\t\tflex-col\n\t\t\t\tgap-4\n\t\t\t\titems-center\n\t\t\t\tabsolute\n\t\t\t\tz-10\n\t\t\t\tbg-white\n\t\t\t\tpy-2\n\t\t\t\tmt-1\n\t\t\t\tbottom-10\n\t\t\t\trounded\n\t\t\t\tborder\n\t\t\t\tborder-grey-beau\n\t\t\t\t" + (dropdown ? 'block' : 'hidden') }, size &&
+                size.map(function (item) { return (React.createElement("p", { key: item, onClick: function () {
+                        rowSizeChange && rowSizeChange(item);
+                        setDropdown(false);
+                        reset();
+                    }, className: "hover:bg-white-azure rounded-lg cursor-pointer py-1 px-4" }, item)); })))));
+};
+exports["default"] = PageRow;
